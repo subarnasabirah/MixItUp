a22d1d4dacf445c53b87133866b905ee2f4826b3
Subarna Akter <subarna769@gmail.com>
2020-09-24 21:04:18 +0600

    mixitup slider done without responsive

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(){
+//     "use strict";
+//     $(".mixitup-items").mixItUp();
+// });
+
+var config = document.querySelector('.mixitup-items');
+var mixer = mixitup(config);
